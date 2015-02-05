da56d4ee1fdc08d5ce69d75f26bbd5a1875727c7
Fabian Maschewski <f.maschewski@mail.de>
2015-02-05 16:54:20 +0100

    updated password generator

--- extension/lib/util/password-generator.js
+++ extension/lib/util/password-generator.js
@@ -1,30 +1,54 @@
 /* globals Services, Uint8Array */
 
+/*
+* This is a utility module to generate secure passwords.
+* Uses the `window.crypto.getRandomValues` function.
+*/
+
 const { Cu } = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
 
 const PASSWORD_LENGTH = 14;
 
-const ASCII_SIZE = 128;
+const ASCII_START = 33;
 
-const ASCII_OFFSET = 33;
+const ASCII_END = 127;
 
+
+/**
+* Generates a new password.
+*
+* @return {String}
+*   A password
+*/
 function generatePassword() {
   const window = Services.appShell.hiddenDOMWindow;
   const bytes = new Uint8Array(PASSWORD_LENGTH);
 
   window.crypto.getRandomValues(bytes);
 
-  const password = bytesToAscii(bytes);
+  const password = bytesToASCII(bytes);
   
   return password;
 }
 
-function bytesToAscii(bytes) {
+/**
+* Converts a byte array to an ASCII string.
+*
+* @param {Array} bytes
+*   An Array of bytes
+* @return {String}
+*   The converted ASCII string
+*/
+function bytesToASCII(bytes) {
   var string = "";
 
+  const interval = ASCII_END - ASCII_START;
+
   for(var i = 0; i < bytes.length; i++) {
-    var charCode = bytes[i] % (ASCII_SIZE - ASCII_OFFSET) + ASCII_OFFSET;
+    const k = Math.floor((bytes[i] - ASCII_START) / interval);
+    const m = bytes[i] - ASCII_START - k * interval;
+    const charCode = ASCII_START + m;
     string += String.fromCharCode(charCode);
   }
 
@@ -32,3 +56,4 @@ function bytesToAscii(bytes) {
 }
 
 exports.generatePassword = generatePassword;
+exports.bytesToASCII = bytesToASCII;
--- extension/test/test-password-generator.js
+++ extension/test/test-password-generator.js
@@ -1,10 +1,42 @@
 var passwordGenerator = require("util/password-generator");
 
 
-exports["test password generatePassword"] = function(assert) {
+exports["test password should generate a password"] = function(assert) {
   const password = passwordGenerator.generatePassword();
   assert.ok(password.length === 14);
 };
 
+exports["test password should generate different passwords"] = function(assert) {
+  const password1 = passwordGenerator.generatePassword();
+  const password2 = passwordGenerator.generatePassword();
+
+  assert.ok(password1 !== password2);
+};
+
+exports["test password should convert bytes to ASCII"] = function(assert) {
+  const asciiBytes = [];
+
+  for(let i = 33; i <= 126; i++) {
+    asciiBytes.push(i);
+  }
+
+  const shiftedAsciiBytes = [];
+
+  for(let i = 0; i < asciiBytes.length; i++) {
+    shiftedAsciiBytes[i] = asciiBytes[i] + 94 * Math.round(Math.random() * 10);
+  }
+
+  const string = passwordGenerator.bytesToASCII(shiftedAsciiBytes);
+
+  var equal = true;
+
+  for(let i = 0; i < asciiBytes.length; i++) {
+    if(string.charCodeAt(i) !== asciiBytes[i]) {
+      equal = false;
+    }
+  }
+
+  assert.ok(equal);
+};
 
 require("sdk/test").run(exports);
